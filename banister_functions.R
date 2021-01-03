40948fa941792ac9751393d506664248a2a45580
Andreas Kjæreng Winther <awi027@uit.no>
2021-01-03 07:40:40 +0100

    added modelT

--- banister_functions.R
+++ banister_functions.R
@@ -16,7 +16,7 @@ convolve_fatigue <- function(training, n, tau, k_2) {
 }
 
 
-banister_function_mae <- function(theta, tl, performance) {
+banister_function_mae <- function(theta, tl, performance, fit = FALSE) {
   int  <- theta[1] # performance baseline
   k1   <- theta[2] # fitness weight
   k2   <- theta[3] # fatigue weight
@@ -36,11 +36,18 @@ banister_function_mae <- function(theta, tl, performance) {
   
   MAE <- mean(abs_errors)
   
-  return(MAE)
+  if (fit == TRUE) {
+    return(MAE)
+  }
+  else {
+    return(perf_hat)
+  }
 }
 
-# Busso function
-busso_function_mae <- function(theta, tl, performance) {
+
+
+
+busso_function_mae <- function(theta, tl, performance, fit = FALSE) {
   int  <- theta[1] # performance baseline
   k1   <- theta[2] # multiplying factor for the positive component of training
   k3   <- theta[3] # multiplying factor for the fatigue factor
@@ -63,6 +70,201 @@ busso_function_mae <- function(theta, tl, performance) {
   
   MAE <- mean(abs_errors)
   
-  return(MAE)
+  if (fit == TRUE) {
+    return(MAE)
+  }
+  else {
+    return(perf_hat)
+  }
 }
 
+
+
+matabuena_function_mae <- function(theta, tl, performance, delay, fit = FALSE) {
+  
+  if (delay == 1) {
+    int  <- theta[1] # performance baseline
+    k1   <- theta[2] # fitness weight
+    k2   <- theta[3] # fatigue weight
+    tau1 <- theta[4] # fitness decay
+    tau2 <- theta[5] # fatigue decay
+    tau3 <- theta[6] # fitness decay
+    tau4 <- theta[7] # fatigue decay
+    
+    fitness <- 0
+    fatigue <- 0
+    
+    for (i in 2:length(tl)) {
+      
+      if (i == 2) {
+        fitness[i] <- exp(-1/tau1)*(tl[i-1] + fitness[i-1]) 
+        fatigue[i] <- exp(-1/tau2)*(tl[i-1] + fatigue[i-1])
+        #perf_hat <- int + k1 * fitness - k2 * fatigue
+      }
+      
+      else { 
+        fitness[i] <- (tl[i-1] + fitness[i-1] - (1/tau3)*fitness[i-2])*exp(-1/tau1)
+        fatigue[i] <- (tl[i-1] + fatigue[i-1] - (1/tau4)*fatigue[i-2])*exp(-1/tau2)
+        #perf_hat <- int + k1 * fitness - k2 * fatigue
+      }
+      
+      perf_hat <- int + k1 * fitness - k2 * fatigue
+      
+    }
+    
+    abs_errors <- abs(performance[!is.na(performance)] - perf_hat[which(!is.na(performance))]) 
+    
+    MAE <- mean(abs_errors)
+    
+    if (fit == TRUE) {
+      return(MAE)
+    }
+    else {
+      return(perf_hat)
+    }
+    
+  }
+  
+  if (delay == 2) {
+    int  <- theta[1] # performance baseline
+    k1   <- theta[2] # fitness weight
+    k2   <- theta[3] # fatigue weight
+    tau1 <- theta[4] # fitness decay
+    tau2 <- theta[5] # fatigue decay
+    tau3 <- theta[6] # fitness decay
+    tau4 <- theta[7] # fatigue decay
+    tau5 <- theta[8] # fitness decay
+    tau6 <- theta[9] # fatigue decay
+    
+    fitness <- 0
+    fatigue <- 0
+    
+    for (i in 2:length(tl)) {
+      
+      if (i == 2) {
+        fitness[i] <- exp(-1/tau1)*(tl[i-1] + fitness[i-1]) 
+        fatigue[i] <- exp(-1/tau2)*(tl[i-1] + fatigue[i-1])
+      }
+      
+      else if (i == 3) {
+        fitness[i] <- (tl[i-1] + fitness[i-1] - (1/tau3)*fitness[i-2])*exp(-1/tau1)
+        fatigue[i] <- (tl[i-1] + fatigue[i-1] - (1/tau4)*fatigue[i-2])*exp(-1/tau2)
+      }
+      
+      else { 
+        fitness[i] <- (tl[i-1] + fitness[i-1] - (1/tau3)*fitness[i-2] - (1/tau5)*fitness[i-3])*exp(-1/tau1)
+        fatigue[i] <- (tl[i-1] + fatigue[i-1] - (1/tau4)*fatigue[i-2] - (1/tau6)*fatigue[i-3])*exp(-1/tau2)
+      }
+      
+      perf_hat <- int + k1 * fitness - k2 * fatigue
+      
+    }
+    
+    abs_errors <- abs(performance[!is.na(performance)] - perf_hat[which(!is.na(performance))]) 
+    
+    MAE <- mean(abs_errors)
+    
+    if (fit == TRUE) {
+      return(MAE)
+    }
+    else {
+      return(perf_hat)
+    }
+    
+  }
+  
+  if (delay == 3) {
+    int  <- theta[1] # performance baseline
+    k1   <- theta[2] # fitness weight
+    k2   <- theta[3] # fatigue weight
+    tau1 <- theta[4] # fitness decay
+    tau2 <- theta[5] # fatigue decay
+    tau3 <- theta[6] # fitness decay
+    tau4 <- theta[7] # fatigue decay
+    tau5 <- theta[8] # fitness decay
+    tau6 <- theta[9] # fatigue decay
+    tau7 <- theta[10] # fitness decay
+    tau8 <- theta[11] # fatigue decay
+    
+    fitness <- 0
+    fatigue <- 0
+    
+    for (i in 2:length(tl)) {
+      
+      if (i == 2) {
+        fitness[i] <- exp(-1/tau1)*(tl[i-1] + fitness[i-1]) 
+        fatigue[i] <- exp(-1/tau2)*(tl[i-1] + fatigue[i-1])
+      }
+      
+      else if (i == 3) {
+        fitness[i] <- (tl[i-1] + fitness[i-1] - (1/tau3)*fitness[i-2])*exp(-1/tau1)
+        fatigue[i] <- (tl[i-1] + fatigue[i-1] - (1/tau4)*fatigue[i-2])*exp(-1/tau2)
+      }
+      
+      else if (i == 4) { 
+        fitness[i] <- (tl[i-1] + fitness[i-1] - (1/tau3)*fitness[i-2] - (1/tau5)*fitness[i-3])*exp(-1/tau1)
+        fatigue[i] <- (tl[i-1] + fatigue[i-1] - (1/tau4)*fatigue[i-2] - (1/tau6)*fatigue[i-3])*exp(-1/tau2)
+      }
+      
+      else {
+        fitness[i] <- (tl[i-1] + fitness[i-1] - (1/tau3)*fitness[i-2] - (1/tau5)*fitness[i-3] - (1/tau7)*fitness[i-4])*exp(-1/tau1)
+        fatigue[i] <- (tl[i-1] + fatigue[i-1] - (1/tau4)*fatigue[i-2] - (1/tau6)*fatigue[i-3] - (1/tau8)*fatigue[i-4])*exp(-1/tau2)
+      }
+      
+      perf_hat <- int + k1 * fitness - k2 * fatigue
+      
+    }
+    
+    abs_errors <- abs(performance[!is.na(performance)] - perf_hat[which(!is.na(performance))]) 
+    
+    MAE <- mean(abs_errors)
+    
+    if (fit == TRUE) {
+      return(MAE)
+    }
+    else {
+      return(perf_hat)
+    }
+    
+  }
+  
+}
+
+
+modelT_mae <- function(theta, tl, performance, fit = FALSE) {
+  k_off <- theta[1]
+  k_on <- theta[2]
+  k_out <- theta[3]
+  
+  int <- performance[!is.na(performance)][1]
+  signal <- 0
+  prod_perf <-int * (1 - exp(-k_off))
+  prod_removal <- int * (1 - exp(-k_off))
+  perf_hat <- int
+  
+  for (i in 2:length(tl)) {
+   
+    signal[i] <- tl[i] + signal[i-1]*exp(-k_on - k_out) 
+    prod_perf[i] <- prod_perf[1] + signal[i] * k_on
+    perf_hat[i] <- prod_perf[i-1] + perf_hat[i-1] * exp(-k_off)
+    
+  }
+  
+  abs_errors <- abs(performance[!is.na(performance)] - perf_hat[which(!is.na(performance))]) 
+  
+  MAE <- mean(abs_errors)
+  
+  if (fit == TRUE) {
+    return(MAE)
+  }
+  else {
+    return(perf_hat)
+  }
+  
+}
+
+
+
+peis <- c(0.0845, 0.0030, 0.0815)
+
+get_performance_modelT(peis, s1$tl, s1$performance)
--- model_comparisons.R
+++ model_comparisons.R
@@ -1,86 +1,61 @@
 # Run *optim* over each athlete in a list
-optim_results_banister <- vector("list", 6)
-for (i in 1:6) {
-  optim_results_banister[[i]] <- optim(par = c(255, 0.10, 0.10, 15, 11), fn = banister_function_mae, tl = subjects_list[[i]][,2], performance = subjects_list[[i]][,3])
-}  
-names(optim_results_banister) <- paste("Subject", 1:6, sep = "")
+optim_results_banister <- lapply(subjects_list, function(x) optim(par = c(255, 0.10, 0.10, 15, 11), fn = banister_function_mae, tl = x[,2], performance = x[,3]))
+optim_results_busso <- lapply(subjects_list, function(x) optim(par = c(255, 0.01, 0.00001, 30, 15, 1), fn = busso_function_mae, tl = x[,2], performance = x[,3]))
+optim_results_matabuena_delay1 <- lapply(subjects_list, function(x) optim(par = c(255, 0.10, 0.10, 10, 10, 5, 5), fn = matabuena_function_mae, tl = x[,2], performance = x[,3], delay = 1, fit = TRUE))
+optim_results_matabuena_delay2 <- lapply(subjects_list, function(x) optim(par = c(255, 0.10, 0.10, 10, 10, 5, 5, 5, 5), fn = matabuena_function_mae, tl = x[,2], performance = x[,3], delay = 2, fit = TRUE))
+optim_results_matabuena_delay3 <- lapply(subjects_list, function(x) optim(par = c(255, 0.10, 0.10, 10, 10, 5, 5, 5, 5, 5, 5), fn = matabuena_function_mae, tl = x[,2], performance = x[,3], delay = 3, fit = TRUE))
+optim_results_modelT <- lapply(subjects_list, function(x) optim(par = c(0.0845, 0.0030, 0.0815), fn = modelT_mae, tl = x[,2], performance = x[,3], fit = TRUE))
 
-get_performance <- function(theta, tl, performance) {
-  int  <- theta[1] # performance baseline
-  k1   <- theta[2] # fitness weight
-  k2   <- theta[3] # fatigue weight
-  tau1 <- theta[4] # fitness decay
-  tau2 <- theta[5] # fatigue decay
-  
-  fitness <- sapply(1:length(tl),
-                    function(n) convolve_training(tl, n, tau1))
-  
-  fatigue <- sapply(1:length(tl),
-                    function(n) convolve_training(tl, n, tau2))
-  
-  int + k1 * fitness - k2 * fatigue
-}
 
 
 for (i in 1:6) {
-  subjects_list[[i]]$perf_hat <- get_performance(optim_results[[i]]$par, banister_parameters[[i]]$tl, banister_parameters[[i]]$performance)
-  
-}
+  subjects_list[[i]]$perf_hat_banister <- get_performance_banister(optim_results_banister[[i]]$par, subjects_list[[i]][,2], performance = subjects_list[[i]][,3])
+  subjects_list[[i]]$perf_hat_busso <- get_performance_busso(optim_results_busso[[i]]$par, subjects_list[[i]][,2], performance = subjects_list[[i]][,3])
+  subjects_list[[i]]$perf_hat_matabuena_delay1 <- get_performance_matabuena(optim_results_matabuena_delay1[[i]]$par, subjects_list[[i]][,2], performance = subjects_list[[i]][,3], delay = 1)
+  subjects_list[[i]]$perf_hat_modelT <- modelT_mae(optim_results_modelT[[i]]$par, subjects_list[[i]][,2], performance = subjects_list[[i]][,3])
+  }
 
-r2 <- sapply(subjects_list, function(x) cor(x$performance, x$perf_hat, use = "complete.obs")^2)  
+
+r2_banister <- sapply(subjects_list, function(x) cor(x$performance, x$perf_hat_banister, use = "complete.obs")^2)  
+r2_busso <- sapply(subjects_list, function(x) cor(x$performance, x$perf_hat_busso, use = "complete.obs")^2)  
+r2_matabuena <- sapply(subjects_list, function(x) cor(x$performance, x$perf_hat_matabuena_delay1, use = "complete.obs")^2)  
+
+r2_banister
+r2_busso
+r2_matabuena
 
 # Visualize results
 
-banister_models <- lapply(banister_models, function(x) {
-  x$Week <- x$Day/7
+subjects_list <- lapply(subjects_list, function(x) {
+  x$week <- x$day/7
   return(x) #Creates a variable called 'Week'
 })
 
-banister_df <- plyr::ldply(banister_models, data.frame)
-colnames(banister_df)[colnames(banister_df)==".id"] <- "Subject"
-banister_plots <- ggplot(banister_df, aes(x = Week, y = Predicted_Performance)) +
-  geom_line(aes(y = Predicted_Performance, colour = "black"), size = 1) +
-  geom_point(aes(y = Performance, colour = "red"), shape = 1) +
-  scale_color_manual("", values = c("black", "red"), labels = c("Predicted Performance", "Actual Performance")) +
-  guides(colour = guide_legend(override.aes = list(linetype = c("solid", "blank"), shape = c(NA, 1)))) +
+subject_df <- plyr::ldply(subjects_list, data.frame)
+
+
+
+colnames(subject_df)[colnames(subject_df)==".id"] <- "Subject"
+subject_plots <- ggplot(subject_df, aes(x = week, y = performance)) +
+  geom_line(aes(y = perf_hat_banister, color = "Predicted performance (Banister)"), size = 1) +
+  geom_line(aes(y = perf_hat_busso, color = "Predicted performance (Busso)"), size = 1) +
+  geom_line(aes(y = perf_hat_matabuena_delay1, color = "Predicted performance (Matabuena w/1 delay)"), size = 1) +
+  geom_point(aes(y = performance, color = "Actual Performance")) +
+  #scale_color_manual("", values = c("red", "blue" ,"green", "black"), labels = c("Predicted performance (Banister)", "Predicted performance (Busso)", "Predicted performance (Matabuena)" ,"Actual performance")) +
+  guides(color = guide_legend(override.aes = list(linetype = c("blank", "solid", "solid", "solid"), shape = c(16, NA, NA, NA)), title = NULL)) +
   ylab("Performance") +
   xlab("Week") +
   scale_x_continuous(breaks = seq(0,16,2)) +
-  geom_label(data = banister_df, aes(x = 2, y = 400, label = paste("italic(R) ^ 2 == ", round(R2, 2))), parse = TRUE, size = 3) +
-  theme_minimal() +
+  #geom_label(data = subject_df, aes(x = 2, y = 400, label = paste("italic(R) ^ 2 == ", round(R2, 2))), parse = TRUE, size = 3) +
+  #theme_minimal() +
   facet_wrap(~ Subject, ncol = 2) 
 
-banister_plots
+subject_plots
 
 
-# Try busso mod
-# Run *optim* over each athlete in a list
-optim_results_busso <- vector("list", 6)
-for (i in 1:6) {
-  optim_results_busso[[i]] <- optim(par = c(255, 0.01, 0.00001, 30, 15, 1), fn = busso_function_mae, tl = subjects_list[[i]][,2], performance = subjects_list[[i]][,3])
-                              
-}  
 
-names(optim_results_busso) <- paste("Subject", 1:6, sep = "")
 
-lapply(optim_results_busso, function(x) x$par)
-lapply(optim_results_banister, function(x) x$par)
 
-lapply(optim_results_busso, function(x) x$value)
-lapply(optim_results_banister, function(x) x$value)
 
-get_performance_busso <- function(theta, tl, performance) {
-  int  <- theta[1] # performance baseline
-  k1   <- theta[2] # fitness weight
-  k2   <- theta[3] # fatigue weight
-  tau1 <- theta[4] # fitness decay
-  tau2 <- theta[5] # fatigue decay
-  
-  fitness <- sapply(1:length(tl),
-                    function(n) convolve_training(tl, n, tau1))
-  
-  fatigue <- sapply(1:length(tl),
-                    function(n) convolve_training(tl, n, tau2))
-  
-  int + k1 * fitness - k2 * fatigue
-}
+
+
